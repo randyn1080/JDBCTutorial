8685ea0bbd72c6f120120d0ea8717ee3f312fb41
Randy Nava <randyn1080@gmail.com>
2025-03-25 16:21:16 -0400

    troubleshooting dependencies - junit not recognized

---
+++ src/test/java/com/randyn1080/jdbctutorial/dao/implementations/TopicDaoImplTest.java
@@ -0,0 +1,11 @@
+package com.randyn1080.jdbctutorial.dao.implementations;
+
+import com.randyn1080.jdbctutorial.dao.interfaces.TopicDao;
+
+import com.randyn1080.jdbctutorial.model.Topic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopicDaoImplTest {
+    private TopicDao topicDao;
+}

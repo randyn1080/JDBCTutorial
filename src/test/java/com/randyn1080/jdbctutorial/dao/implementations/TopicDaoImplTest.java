25d31e6acff3a141dc17bdba0507d3aae378b84a
Randy Nava <randyn1080@gmail.com>
2025-03-25 16:31:42 -0400

    dependencies fixed - halfway through implementing TopicDao tests

--- src/test/java/com/randyn1080/jdbctutorial/dao/implementations/TopicDaoImplTest.java
+++ src/test/java/com/randyn1080/jdbctutorial/dao/implementations/TopicDaoImplTest.java
@@ -1,11 +1,43 @@
 package com.randyn1080.jdbctutorial.dao.implementations;
 
+import com.randyn1080.jdbctutorial.dao.Implementations.TopicDaoImpl;
 import com.randyn1080.jdbctutorial.dao.interfaces.TopicDao;
-
 import com.randyn1080.jdbctutorial.model.Topic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TopicDaoImplTest {
+
     private TopicDao topicDao;
+
+    @BeforeEach
+    void setUp() {
+        topicDao = new TopicDaoImpl();
+    }
+
+    @Test
+    void testSaveAndFindById() {
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+
+    @Test
+    void testDelete() {
+
+    }
 }
